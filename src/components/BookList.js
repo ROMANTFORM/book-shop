c0d81ca31dfc3f26cc320eb3ee29c9bd2e5ce38c
Romantform <romantform@gmail.com>
2023-12-07 13:07:42 +0100

    db.json bookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,26 @@
-import pic from '../images/exemple.jpg';
+import { useEffect, useState } from 'react';
+
+import defaultImg from '../images/defaultIMG.png';
 import { IoIosSearch } from "react-icons/io";
 
 
+
 export default function BookList(){
+
+    const [fetchData, setFetchData] = useState();
+
+    const api = 'http://localhost:8080/books';
+
+    useEffect(() => {
+        fetch(api)
+        .then(res => res.json())
+        .then(data => setFetchData(data))
+    }, [api])
+
     return(
         <div className="booklist-section section">
             <div className="container">
+
                 <div className="filter__container">
                     <div className='filter__search-wrap'>
                         <input className='filter__search-inpt' type='text' name='search'/>
@@ -20,68 +35,26 @@ export default function BookList(){
                     </div>
                 </div>
 
-                <div className="book__list">
-                    
-                    <div className="book__item">
-                        <div className='book__item-img-wrap'>
-                           <img src={pic} alt='pic' className="book__item-img"/> 
-                        </div>
-                        
-                        <div className="book__item-info">
-                            <h3>Name: </h3>
-                            <h4>Author: </h4>
-                            <div className="book__item-controls">
-                                <span>Price:</span>
-                                <button className="btn book__item-btn">View</button>
+                <div className="book__list"> 
+                    {fetchData?.map(item =>(
+                        <div className="book__item" key={item.id}>
+                            <div className='book__item-img-wrap'>
+                            <img src={item.image ? item.image : defaultImg} alt='pic' className="book__item-img"/> 
+                            </div>
+                            
+                            <div className="book__item-info">
+                                <h3>{item.title}</h3>
+                                <h5>Author: {item.author}</h5>
+                                <div className="book__item-controls">
+                                    <span>Price: {item.price}$</span>
+                                    <button className="btn book__item-btn">View</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="book__item">
-                        <div className='book__item-img-wrap'>
-                           <img src={pic} alt='pic' className="book__item-img"/> 
-                        </div>
-                        
-                        <div className="book__item-info">
-                            <h3>Name: </h3>
-                            <h4>Author: </h4>
-                            <div className="book__item-controls">
-                                <span>Price:</span>
-                                <button className="btn book__item-btn">View</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="book__item">
-                        <div className='book__item-img-wrap'>
-                           <img src={pic} alt='pic' className="book__item-img"/> 
-                        </div>
-                        
-                        <div className="book__item-info">
-                            <h3>Name: </h3>
-                            <h4>Author: </h4>
-                            <div className="book__item-controls">
-                                <span>Price:</span>
-                                <button className="btn book__item-btn">View</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="book__item">
-                        <div className='book__item-img-wrap'>
-                           <img src={pic} alt='pic' className="book__item-img"/> 
-                        </div>
-                        
-                        <div className="book__item-info">
-                            <h3>Name: </h3>
-                            <h4>Author: </h4>
-                            <div className="book__item-controls">
-                                <span>Price:</span>
-                                <button className="btn book__item-btn">View</button>
-                            </div>
-                        </div>
-                    </div>
-                    
+                    ))}                 
                 </div>
-            </div>
-            
+
+            </div>    
         </div>
     )
-}
+};
